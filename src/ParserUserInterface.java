2b14324340526081c058c6b3748326171e0e71cf
dakafak <coreyf95@gmail.com>
2018-11-16 19:43:53 -0600

    added base project and classes

---
+++ src/ParserUserInterface.java
@@ -0,0 +1,9 @@
+public class ParserUserInterface {
+
+    public static void main(String[] args){
+        if(args != null && args.length >= 1){
+            String projectToParseParentDirectory = args[0];// Parent directory containing all project files
+        }
+    }
+
+}
---
+++ src/parser/parser_api/ParserInterface.java
@@ -0,0 +1,13 @@
+package parser.parser_api;
+
+import parser.nodes.ProjectNode;
+
+import java.io.File;
+
+public class ParserInterface {
+
+    public static ProjectNode getParsedProject(final File parentFile){
+        return null;
+    }
+
+}
